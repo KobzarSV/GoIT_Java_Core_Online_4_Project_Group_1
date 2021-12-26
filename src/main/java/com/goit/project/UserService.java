4c9a002e2be71eda845ddea634ed0db3320e26e0
NefedovAndry <nefedov_andry@bk.ru>
2021-12-26 17:55:05 +0200

    fix UserService - add getUsersIdWithPresentScheduler() fix UserStorage - change HashMap() to ConcurrentHashMap()

--- src/main/java/com/goit/project/UserService.java
+++ src/main/java/com/goit/project/UserService.java
@@ -1,57 +1,64 @@
 package com.goit.project;
 
+import java.util.List;
+
 public class UserService {
+    UserStorage userStorage = UserStorage.getInstance();
 
     public void createUser(int userId) {
-        UserStorage.add(new User(userId));
+        userStorage.add(new User(userId));
     }
 
     public void setBank(int userId, String bank) {
-        UserStorage.get(userId).setBank(bank);
+        userStorage.get(userId).setBank(bank);
     }
 
     public void setRounding(int userId, int rounding) {
-        UserStorage.get(userId).setRounding(rounding);
+        userStorage.get(userId).setRounding(rounding);
     }
 
     public void setUsd(int userId, boolean usd) {
-        UserStorage.get(userId).setUsd(usd);
+        userStorage.get(userId).setUsd(usd);
     }
 
     public void setEur(int userId, boolean eur) {
-        UserStorage.get(userId).setEur(eur);
+        userStorage.get(userId).setEur(eur);
     }
 
     public void setRub(int userId, boolean rub) {
-        UserStorage.get(userId).setRub(rub);
+        userStorage.get(userId).setRub(rub);
     }
 
     public void setScheduler(int userId, boolean scheduler) {
-        UserStorage.get(userId).setScheduler(scheduler);
+        userStorage.get(userId).setScheduler(scheduler);
     }
 
     public void setSchedulerTime(int userId, int time) {
-        UserStorage.get(userId).setSchedulerTime(time);
+        userStorage.get(userId).setSchedulerTime(time);
     }
 
     public String getBank(int userId) {
-        return UserStorage.get(userId).getBank();
+        return userStorage.get(userId).getBank();
     }
 
     public Integer getRounding(int userId) {
-        return UserStorage.get(userId).getRounding();
+        return userStorage.get(userId).getRounding();
     }
 
     public boolean[] getCurrencies(int userId) {
-        return new boolean[]{UserStorage.get(userId).isUsd(), UserStorage.get(userId).isEur(), UserStorage.get(userId).isRub()};
+        return new boolean[] {userStorage.get(userId).isUsd(), userStorage.get(userId).isEur(), userStorage.get(userId).isRub()};
     }
 
     public boolean getScheduler(int userId) {
-        return UserStorage.get(userId).isScheduler();
+        return userStorage.get(userId).isScheduler();
     }
 
     public Integer getSchedulerTime(int userId) {
-        return UserStorage.get(userId).getSchedulerTime();
+        return userStorage.get(userId).getSchedulerTime();
+    }
+
+    public List<Integer> getUsersIdWithPresentScheduler(int time) {
+        return userStorage.getUsersIdWithPresentScheduler(time);
     }
 
 }
--- src/main/java/com/goit/project/UserStorage.java
+++ src/main/java/com/goit/project/UserStorage.java
@@ -1,21 +1,46 @@
 package com.goit.project;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class UserStorage {
-    public static HashMap<Integer, User> userSettings;
+    private static volatile UserStorage instance; // Singleton
+    private ConcurrentHashMap<Integer, User> userSettings;
 
-    public UserStorage() {
-        userSettings = new HashMap<>();
+    private UserStorage() {
+        userSettings = new ConcurrentHashMap<>();
     }
 
-    public static void add(User user) {
+    public static UserStorage getInstance() { //«блокировка с двойной проверкой» (Double-Checked Locking)
+        UserStorage result = instance;
+        if (result != null) {
+            return result;
+        }
+        synchronized(UserStorage.class) {
+            if (instance == null) {
+                instance = new UserStorage();
+            }
+            return instance;
+        }
+    }
+
+    public void add(User user) {
         userSettings.put(user.getId(), user);
     }
 
-    public static User get(int userId) {
+    public User get(int userId) {
         return userSettings.get(userId);
     }
 
+    public List<Integer> getUsersIdWithPresentScheduler(int time) {
+        List<Integer> result = new ArrayList<>();
+        for (User user : userSettings.values()) {
+            if (user.getSchedulerTime() == time) {
+                result.add(user.getId());
+            }
+        }
+        return result;
+    }
 
 }
